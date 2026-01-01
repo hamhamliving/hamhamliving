c2635a1a3235eb888b89aa698111d5c1724eeb14
hamham <lsubig3@gmail.com>
2026-01-01 17:17:32 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,48 +1,19 @@
 const productData = [
     
     {
-        "id": "001",
-        "title": "낭만 한도초과 - 노래 나오는 주문제작 마그넷",
-        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
-        "isRocket": true
-    },
-        {
         "id": "002",
-        "title": "방귀방귀조아",
-        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
+        "title": "쿠쿠 - 스테인리스 아이편한 초음파 가습기",
+        "imageUrl": "https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/1764107998875212-2f0cb81a-8808-46be-a277-bebd44af7ad0.jpg",
+        "link": "https://link.coupang.com/a/djU2a9",
         "isRocket": true
     },
             {
-        "id": "003",
-        "title": "test23112321",
+        "id": "001",
+        "title": "낭만 한도초과 - 노래나오는 주문제작 마그넷, 자석",
         "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
-        "isRocket": true
-    },
-            {
-        "id": "001234",
-        "title": "test3",
-        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
-        "isRocket": true
-    },
-            {
-        "id": "005",
-        "title": "test2",
-        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
-        "isRocket": true
-    },
-            {
-        "id": "test",
-        "title": "tes123t11",
-        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
-        "link": "https://link.coupang.com/a/djEsEP",
-        "isRocket": true
+        "link": "https://link.coupang.com/a/djU7as",
+        "isRocket": false
     }
 
     
-
 ];
