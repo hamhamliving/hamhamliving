62b381f880d273c1e450a9f442bdae2c888a7957
hamham <lsubig3@gmail.com>
2026-02-07 21:48:42 +0900

    Update data.js

--- data.js
+++ data.js
@@ -20,16 +20,23 @@ const productData = [
         "link": "https://link.coupang.com/a/djU2a9",
         "isRocket": true
     },
-            {
+    {
         "id": "001",
         "title": "낭만 한도초과 - 노래나오는 주문제작 마그넷, 자석",
         "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
         "link": "https://link.coupang.com/a/djU7as",
         "isRocket": false
+    },
+    {
+        "id": "05",
+        "title": "https://link.coupang.com/a/dH06zz",
+        "imageUrl": "thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/4f98/167c13f0f1c5014736096035aa5a566aa048fa28a642463130b28e7a6d7e.jpg",
+        "link": "https://link.coupang.com/a/dH06zz",
+        "isRocket": true
     }
-
     
 
 ];
+
 
 
